e0d25d88cc0bb9a2bfcd33132380af0b4b087b66
csibivili <csibi.vili@gmail.com>
2024-02-05 10:12:51 +0100

    feat: add tailwind

--- packages/web/pages/index.tsx
+++ packages/web/pages/index.tsx
@@ -1,4 +1,4 @@
-import Head from "next/head";
+import Head from "next/head"
 
 export default function Home() {
   return (
@@ -10,7 +10,8 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
+        <h1 className="text-3xl font-bold underline">Hello world!</h1>
       </main>
     </>
-  );
+  )
 }
