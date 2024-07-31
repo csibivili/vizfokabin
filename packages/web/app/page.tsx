bb5ce69b58fccc96ead5efaeb89e4a1f1297742e
csibivili <csibi.vili@gmail.com>
2024-07-31 22:29:41 +0200

    feat: header/footer basics

--- packages/web/app/page.tsx
+++ packages/web/app/page.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image"
+import Link from "next/link"
 
 import AirbnbButton from "@/components/airbnbButton"
 import BookingButton from "@/components/bookingButton"
@@ -6,15 +7,17 @@ import BookingButton from "@/components/bookingButton"
 export default function Home() {
   return (
     <div className="bg-gray-950">
-      <header className="max-w-screen-2xl w-full h-16 bg-gray-950 px-10 flex items-center mx-auto">
-        <div>
-          <Image src="/logo.svg" alt="Vízfő Kabin" width={60} height={60} />
+      <header className="w-full h-24 bg-gray-950 fixed z-50">
+        <div className="max-w-screen-2xl mx-auto w-full flex items-center h-full">
+          <div>
+            <Image src="/logo.svg" alt="Vízfő Kabin" width={120} height={120} />
+          </div>
         </div>
         {/* <p className="font-bold uppercase text-xl">Vízfő Kabin</p> */}
       </header>
 
       <main className="relative overflow-hidden">
-        <div className="h-screen bg-gray-700" style={{ marginTop: "-4rem" }}>
+        <div className="h-screen bg-gray-700">
           <div className="absolute inset-0 max-w-screen-2xl justify-self-center">
             <div className="absolute inset-0 bg-black opacity-50" />
             <Image
@@ -26,13 +29,13 @@ export default function Home() {
             />
           </div>
           <div className="absolute inset-0 grid grid-rows-3 max-w-screen-2xl justify-self-center w-full">
-            <div className="row-span-3 px-10 flex flex-col pb-4">
+            <div className="row-span-3 px-10 flex flex-col pb-16">
               <div className="flex flex-col mt-auto mb-4 md:max-w-screen-md">
-                <h2 className="text-white text-5xl font-bold">
+                <h2 className="text-white text-3xl font-bold md:text-5xl">
                   Tarts egy kis szünetet, és lazíts egyet a{" "}
                   <span className="underline text-red-100">Bükkben</span>
                 </h2>
-                <h3 className="text-white text-2xl mt-4 italic font-thin">
+                <h3 className="text-white text-xl mt-4 italic font-thin">
                   Kirándulj a környéken, vagy merülj el a jacuzziban miközben
                   élvezed a végtelen nyugalmat
                 </h3>
@@ -46,8 +49,21 @@ export default function Home() {
         </div>
       </main>
 
-      <footer className="w-full h-96 bg-black">
-        <p>Footer</p>
+      <footer className="w-full md:h-96 bg-gray-800 py-8">
+        <div className="max-w-screen-2xl mx-auto grid md:grid-cols-3 text-white text-lg">
+          <div className="text-center flex flex-col justify-center"></div>
+          <div className="text-center flex flex-col justify-center">
+            <p className="font-bold">Vízfő Kabin</p>
+            <p>3425 Sály, Vízfő telep 662/5</p>
+            <p className="font-bold mt-2">Csibi Mátyás</p>
+            <Link href="mailto:info@vizfokabin.com">
+              <p className="underline">info@vizfokabin.com</p>
+            </Link>
+          </div>
+          <div>
+            <Image src="/logo.svg" alt="Vízfő Kabin" width={400} height={400} />
+          </div>
+        </div>
       </footer>
     </div>
   )
